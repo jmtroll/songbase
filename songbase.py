66f85888f9aa1c9053a87d35d602b7fcf5707dde
Jamie Troll <jmtroll@udel.edu>
2017-11-09 10:55:18 -0500

    Flask and template exercises

--- songbase.py
+++ songbase.py
@@ -1,13 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-        return '<h1>hello world!!!</h1'
+        # return '<h1>hello world!!!</h1'
+        return render_template('index.html')
+
+@app.route('/mysong')
+def mysong():
+        return render_template('mysong.html')
 
 @app.route('/user/<string:name>/')
 def get_user(name):
-        return 'hello %s your age is %i' % (name,3)
+        # return 'hello %s your age is %i' % (name,3)
+        return render_template('user.html',user_name=name)
+
+@app.route('/songs')
+def get_all_songs():
+        songs = [
+        'song1',
+        'song2',
+        'song3'
+        ]
+        return render_template('songs.html', songs=songs)
 
 @app.route('/users')
 def show_all_users():
